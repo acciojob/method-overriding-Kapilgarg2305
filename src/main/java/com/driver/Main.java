ecf65713da992e0aa7b6ca9b9026d7909d9d814e
Kapil <kapilgargymca@gmail.com>
2024-03-10 00:05:52 +0530

    ist

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String args[]){
+    A obj1=new B();
+    B obj2=new B();
+    System.out.println(A.meth());
+        System.out.print(B.meth());
+    }
+  public static class A{
+      public static String meth(){
+          return "Invoking method from class A";
+
+      }
+  }
+  public static class B extends A{
+      public static String meth(){
+          return "Method is overridden in Extended class B";
+
+      }
+  }
 }
